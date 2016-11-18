d876380d5caf9d479332eecb6477a9293579c5ac
Hanggi <hanggicrown@gmail.com>
2016-11-18 19:41:11 +0900

    start async with pm2

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-// const Koa = require('koa');
-// const app = new Koa();
-//
-// const logger = require('koa-logger');
-//
-//
-// app.use(logger());
-//
-// // app.use((ctx, next) => {
-// //     const start = new Date();
-// //     return next().then(() => {
-// //         const ms = new Date() - start;
-// //         console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-// //     });
-// // });
-//
-// // app.use(async function(ctx, next) {
-// //   const start = new Date();
-// //   await next();
-// //   const ms = new Date() - start;
-// //   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-// // });
-//
-// app.use(ctx => {
-//     ctx.body = 'listening @ port: 3210';
+const Koa = require('koa');
+const app = new Koa();
+
+const logger = require('koa-logger');
+
+
+app.use(logger());
+
+app.use(async (ctx, next) => {
+  try {
+    await next(); // wait until we execute the next function down the chain, then continue;
+  } catch (err) {
+    ctx.body = { message: err.message };
+    ctx.status = err.status || 500;
+  }
+});
+
+// app.use((ctx, next) => {
+//     const start = new Date();
+//     return next().then(() => {
+//         const ms = new Date() - start;
+//         console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+//     });
 // });
-//
-//
-//
-// app.listen(3210);
+
+// app.use(async function(ctx, next) {
+//   const start = new Date();
+//   await next();
+//   const ms = new Date() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+// });
+
+app.use(ctx => {
+    ctx.body = 'listening @ port: 3210';
+});
 
 
-imports Koa from 'koa';
+
+app.listen(3210);
