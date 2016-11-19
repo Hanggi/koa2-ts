cb8ceb700f45a26fc871d4d0423896b674b63faa
Hanggi <hanggicrown@gmail.com>
2016-11-19 17:55:35 +0900

    aa

--- app.js
+++ app.js
@@ -3,26 +3,38 @@ const app = new Koa();
 
 const path = require('path');
 
+// koa-logger
 const logger = require('koa-logger');
-const render = require('koa-ejs');
-const co = require('co');
-const router = require('koa-router')();
-const serve = require('koa-static');
-app.use(serve(__dirname + '/public'));
+app.use(logger());   // deprecated
 
-router.get('/qwe', async function (ctx, next) {
-    console.log('123')
 
-    await ctx.render('home', {
-        name: 'koa'
-    });
-});
+// const co = require('co');
+
 
 app
     .use(router.routes())
     .use(router.allowedMethods());
 
-app.use(logger());
+// koa-commpress
+const compress = require('koa-compress');
+// app.use(compress({
+//     filter: function (content_type) {
+//         return /text/i.test(content_type)
+//     },
+//     threshold: 2048,
+//     flush: require('zlib').Z_SYNC_FLUSH
+// }));
+
+// koa-static
+const serve = require('koa-static');
+app.use(serve(__dirname + '/public'));   //deprecated
+
+// koa-favicon
+const favicon = require('koa-favicon');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// koa-ejs
+const render = require('koa-ejs');
 render(app, {
     root: path.join(__dirname, 'views'),
     layout: null,
@@ -32,6 +44,15 @@ render(app, {
 });
 app.context.render = co.wrap(app.context.render);
 
+// koa-router
+const router = require('koa-router')();
+router.get('/qwe', async function (ctx, next) {
+    console.log('123')
+
+    await ctx.render('home', {
+        name: 'koa2 '
+    });
+});
 
 
 app.use(async (ctx, next) => {
