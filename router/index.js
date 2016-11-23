86aa2cc6edad4a2cdd6d3888cfd60e6f9abe91a5
Hanggi <hanggicrown@gmail.com>
2016-11-23 19:51:38 +0900

    add start

--- router/index.js
+++ router/index.js
@@ -6,6 +6,10 @@ module.exports = (app) => {
         .get('/', async (ctx, next) => {
             ctx.body = 'koa2 build';
         })
+        .get('/test', async (ctx, next) => {
+            console.log(ctx.query)
+            ctx.body = `This is the test page GET:${ctx.query}`;
+        })
         .get('/qwe', async (ctx, next) => {
 
             await ctx.render('home', {
