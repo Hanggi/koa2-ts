0403b86b4d084f9581aabf6f13c508e26d87618e
Hanggi <hanggicrown@gmail.com>
2017-07-09 18:10:57 +0900

    nn

--- webhook.js
+++ webhook.js
@@ -5,7 +5,7 @@ let handler = createHandler({path: '/gitpush', secret: 'fangda'});
 function run_cmd(cmd, args, callback) {
     let spawn = require('child_process').spawn;
     let child = spawn(cmd, args);
-    var resp = "ok!";
+    var resp = "ok!\n";
 
     child.stdout.on('data', function(buffer) {resp += buffer.toString();})
     child.stdout.on('end', function() {callback(resp)})
