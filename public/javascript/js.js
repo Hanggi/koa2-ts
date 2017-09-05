4ef4a7a8045fc0de8a5392314831fddd566204e7
Hanggi <hanggicrown@gmail.com>
2017-09-05 16:12:36 +0900

    fff

--- public/javascript/js.js
+++ public/javascript/js.js
@@ -8,56 +8,71 @@
 /*
  * 字符串
  */
-let str;
-let str2;
-console.log("Javascript 教程：")
-// 基本声明
-console.log("基本字符串：");
-str = 'This is a string. 👏';
-str2 = "This is the 2nd string. 💁";
-
-console.log(str);
-console.log(str2);
-
-// 嵌套
-console.log("\n引号嵌套：")
-str = "It's six o'clock.";
-str2 = 'Remember to say "please" and "thank you."';
-
-console.log(str);
-console.log(str2);
-
-str = 'It\'s six o\'clock.';
-str2 = "Remember to say \"please\" and \"thank you.\"";
-
-console.log(str);
-console.log(str2);
-
-// 长度
-console.log("\n字符串长度：")
-console.log('"caterpillar".length;');
-console.log("caterpillar".length);
-console.log('"THE KIDS".toLowerCase();');
-console.log("THE KIDS".toLowerCase());
-console.log('"I wish I were big.".toUpperCase();');
-console.log("I wish I were big.".toUpperCase());
-console.log('"   but keep the middle spaces   ".trim();');
-console.log("   but keep the middle spaces   ".trim());
-
-String.prototype.hanggi = function () {console.log("String has a custom function of hanggi.");}
-console.log('\n"This is a custom function of String.".hanggi();');
-"This is a custom function of String.".hanggi();
-
-// var, let, const
-console.log("\nES6中的变量声明:");
 {
-    let a = 10;
-    var b = 1;
+    let str;
+    let str2;
+    console.log("Javascript 教程：")
+    // 基本声明
+    console.log("基本字符串：");
+    str = 'This is a string. 👏';
+    str2 = "This is the 2nd string. 💁";
+
+    console.log(str);
+    console.log(str2);
+
+    // 嵌套
+    console.log("\n引号嵌套：")
+    str = "It's six o'clock.";
+    str2 = 'Remember to say "please" and "thank you."';
+
+    console.log(str);
+    console.log(str2);
+
+    str = 'It\'s six o\'clock.';
+    str2 = "Remember to say \"please\" and \"thank you.\"";
+
+    console.log(str);
+    console.log(str2);
+
+    // 长度
+    console.log("\n字符串长度：")
+    console.log('"caterpillar".length;');
+    console.log("caterpillar".length);
+    console.log('"THE KIDS".toLowerCase();');
+    console.log("THE KIDS".toLowerCase());
+    console.log('"I wish I were big.".toUpperCase();');
+    console.log("I wish I were big.".toUpperCase());
+    console.log('"   but keep the middle spaces   ".trim();');
+    console.log("   but keep the middle spaces   ".trim());
+
+    String.prototype.hanggi = function () {console.log("String has a custom function of hanggi.");}
+    console.log('\n"This is a custom function of String.".hanggi();');
+    "This is a custom function of String.".hanggi();
+
+    // 字符串输出&连接
+    let a = 123;
+    console.log("Value a is equal to " + a + "!!!");
+    console.log(`Value a is equal to ${a}!!!`);
+
+    // var, let, const
+    console.log("\nES6中的变量声明:");
+    {
+        let a = 10;
+        var b = 1;
+    }
+    try {
+        console.log(a);
+    } catch (e) {
+        console.log(`a is not defined!!`);
+    }
+    console.log(b);
 }
-try {
-    console.log(a);
-} catch (e) {
-    console.log(`a is not defined!!`);
-}
-console.log(b);
-  
+
+/*
+ * 数量
+ */
+var num = 123;
+let float = 0.333;
+const PI = 3.1415926;
+
+console.log(`num: ${num}, float: ${float}, PI: ${PI}`);
--- public/javascript/js.md
+++ public/javascript/js.md
@@ -37,10 +37,47 @@ js 里虽然没有 class 声明类方法，但是`所有类型都可以视为对
 "This is a custom function of String.".hanggi();
 ```
 
+字符串的输出和连接也非常方便
+
+```js
+// 在 ES5 版本中我们可以简单使用引号和加好
+var a = 123
+console.log("Value a is equal to " + a + "!!!");
+// ES6 给我们带来了更方便的功能。
+console.log(`Value a is equal to ${a}!!!`);
+```
+
 #### 【穿插内容】var, let, const 用法
 
 在较早版本的 js 中是只有 `var` 声明变量。首先 js 本身是弱类型语言，所以在声明变量是可以简单使用 `var a = 1` 这种表达式，语言本身会判断类型并进行转换。
 
 而在最新版本的 js 中增加了 let 和 const。目的在于解决 `var` 本身的缺陷。
+即，`var` 不支持块级声明。
 
+```js
+{
+  let a = 10;
+  var b = 1;
+}
 
+a // ReferenceError: a is not defined.
+b // 1
+```
+这种特性可以解决很多问题，具体内容虚招ES6相关介绍。
+
+# 数量
+
+js 是弱类型语言，他不区分整型，浮点型等。所以你可以用以下方式简单声明：
+
+```js
+var num = 123;
+let float = 0.333;
+const PI = 3.1415926;
+```
+
+并对他们进行加减乘除，你完全可以把它当做计算器来使用。
+
+```js
+10 + 3.1415;
+1 + (4 / 3)
+```
