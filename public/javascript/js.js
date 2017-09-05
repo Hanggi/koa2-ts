259e588f6e236b00b18bcf51b572cac7ccb248b1
Hanggi <hanggicrown@gmail.com>
2017-09-05 14:58:31 +0900

    tutorial

---
+++ public/javascript/js.js
@@ -0,0 +1,21 @@
+
+/*
+ * 字符串
+ */
+let str;
+let str2;
+// 基本声明
+console.log("基本字符串：");
+str = 'This is a string. 👏';
+str2 = "This is the 2nd string. 💁";
+
+console.log(str);
+console.log(str2);
+
+// 嵌套
+console.log("引号嵌套：")
+str = "It's six o'clock.";
+str2 = 'Remember to say "please" and "thank you."';
+
+console.log(str);
+console.log(str2);
---
+++ public/javascript/js.md
@@ -0,0 +1,17 @@
+# Javascript 基本教程
+
+### 字符串
+
+js 可以使用 ' ' 或者 " " 来表示字符串。
+
+```js
+str = 'This is a string. 👏';
+str2 = "This is the 2nd string. 💁";
+```
+
+并且他们之间是可以互相嵌套的。
+
+```js
+str = "It's six o'clock.";
+str2 = 'Remember to say "please" and "thank you."';
+```
