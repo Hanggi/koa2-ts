db6775d1a39ee975ddeaf3f51bbca7d8d0cc8f8e
Hanggi <hanggicrown@gmail.com>
2017-09-17 00:26:15 +0900

    bc

--- router/index.js
+++ router/index.js
@@ -2,6 +2,20 @@
 const router = require('koa-router')();
 const send = require('koa-send');
 
+const request = require('request');
+
+function doRequest(url) {
+    return new Promise(function (resolve, reject) {
+      request(url, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+          resolve(body);
+        } else {
+          reject(error);
+        }
+      });
+    });
+  }
+
 module.exports = (app) => {
     router
         .get('/', async (ctx, next) => {
@@ -14,6 +28,23 @@ module.exports = (app) => {
             let query = JSON.stringify(ctx.query);
             console.log(typeof(query));
             ctx.body = `This is the test page GET:${query}`;
+        })
+        .get('/bitcoin', async (ctx, next) => {
+            let coinone = await doRequest('https://api.coinone.co.kr/ticker/');
+            let bitflyer = await doRequest('https://lightning.bitflyer.jp/v1/getprices');
+            let viabtc = await doRequest('https://api.viabtc.com/v1/market/ticker?market=BTCCNY');
+
+            console.log(JSON.parse(bitflyer)[0].rate)
+            bfRes = JSON.stringify(JSON.parse(bitflyer)[0].rate)
+            await ctx.render('bitcoin', {
+                co: JSON.parse(coinone).last,
+                bf: bfRes,
+                vb: JSON.parse(viabtc).data.ticker.last
+            });
+            
+            // ctx.body = {
+            //     co: JSON.parse(res.last)
+            // }
         })
         .get('/adm*', async (ctx, next) => {
             console.log(__dirname);
