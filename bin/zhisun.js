1a39f67ef69c5818f57c7512d2ee745d7a84b357
Hanggi <hanggicrown@gmail.com>
2017-12-20 16:55:27 +0900

    vvv

---
+++ bin/zhisun.js
@@ -0,0 +1,201 @@
+const nodemailer = require('nodemailer');
+const schedule = require('node-schedule');
+var smtpTransport = require('nodemailer-smtp-transport');
+// const xoauth2 = require('xoauth2');
+const request = require('request');
+const Koa = require('koa');
+const app = new Koa();
+
+
+
+
+
+setInterval(()=>{
+    request("https://api.coinone.co.kr/trades?currency=bch", (err, res, body) => {
+    if (err) {
+        console.log(err)
+    }
+    let data = JSON.parse(body).completeOrders;
+    // console.log(data.length)
+
+    let max = Math.max.apply(Math, data.slice().map(function (o) {return o.price;}))
+    // console.log("一小时最高价：" + max)
+
+    let now = data[data.length - 1].price;
+    // console.log("目前价格:" + now)
+
+    let rate = now / max;
+    console.log("涨跌百分比：" + rate*100 + "%")
+    // console.log(typeof rate)
+
+    if (rate < 0.9) {
+        console.log("到达止损点！开始止损");
+
+    }
+
+    });
+}, 800);
+
+
+
+
+// var last_price = -1;
+// var last_bch_price = -1;
+
+// var transporter = nodemailer.createTransport(smtpTransport({
+//     host: "smtp.qq.com", // 主机
+//     secure: true, // 使用 SSL
+//     port: 465, // SMTP 端口
+//     auth: {
+//       user: "271335064@qq.com", // 账号
+//       pass: "xgebevpovhhccbbj" // 密码
+//     }
+// }));
+
+// let doSendEmail = (price, title, data) => {
+//     let mailOptions = {
+//         from: '271335064@qq.com', // sender address
+//         // to: 'hanggicrown@gmail.com', // list of receivers
+//         to: '2745490330@qq.com',
+//         // to: 'hanggi@seoul.ac.kr',
+//         subject: `${title} [${price}]`, // Subject line
+//         text: '!!Hello world ✔', // plaintext body
+//         html: `目前价格为:<b>₩${price}</b> 从 ₩${data.last} ${data.updown > 0 ? "↑上涨↑": "↓下跌↓"}
+//             <br>
+//             最高价：₩${data.data.max_price}，最低价：₩${data.data.min_price}
+//         ` // html body
+    
+//     };
+    
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if(error){
+//             console.log(error);
+//         }else{
+//             console.log('Message sent: ' + info.response);
+//         }
+//     });
+// }
+// let lastTime = 0
+// let rounding = (p) => parseInt(p / 50000);
+
+// let heat = 0;
+
+// let scheduleCronstyle = () => {
+//     // schedule.scheduleJob('1 * * * * *', function(){
+//         setInterval(()=>{
+//             request("https://api.coinone.co.kr/trades?currency=bch", (err, res, body) => {
+//                 if (err) {
+//                     console.log(err)
+//                 }
+//                 let data = JSON.parse(body).completeOrders;
+//                 let now = data[data.length - 1].timestamp;
+//                 let limit = now - 60;
+//                 let qty = 0;
+//                 // console.log(now)
+//                 for (let i = data.length - 1; i > 0; i--) {
+//                     // console.log(typeof data)
+//                     // console.log(i)
+//                     if (Number(data[i].timestamp) < limit) {
+//                         break;
+//                     }
+//                     qty += Number(data[i].qty);
+//                 }
+//                 qty = Math.round(qty)
+//                 console.log("前一分钟交易量：" + qty)
+//                 // console.log(heat)
+//                 if (qty > 160) {
+//                     let mailOptions = {
+//                         from: '271335064@qq.com', // sender address
+//                         // to: 'hanggicrown@gmail.com', // list of receivers
+//                         to: '2745490330@qq.com',
+//                         // to: 'hanggi@seoul.ac.kr',
+//                         subject: `交易量突然上升! ${qty}`, // Subject line
+//                         text: '!!Hello world ✔', // plaintext body
+//                         html: `交易量提升警报！` // html body
+//                     };
+
+//                     let interval = new Date() - lastTime;
+//                     if (heat < 20)
+//                         heat++;
+
+//                     if (interval > 10000 * heat) {
+//                         lastTime = new Date()
+                        
+//                         transporter.sendMail(mailOptions, function(error, info){
+//                             if(error){
+//                                 console.log(error);
+//                             }else{
+//                                 console.log('Message sent: ' + info.response);
+//                             }
+//                         });
+//                         mailOptions.to = "271335064@qq.com"
+//                         transporter.sendMail(mailOptions, function(error, info){
+//                             if(error){
+//                                 console.log(error);
+//                             }else{
+//                                 console.log('Message sent: ' + info.response);
+//                             }
+//                         });
+//                     }
+//                 }else {
+//                     if (heat > 0)
+//                         heat--;
+//                 }
+
+//             });
+//         }, 3000);
+//     // }); 
+//     setInterval(()=>{
+
+//         console.log('scheduleCronstyle:' + new Date());
+//         // let bithumb = await doRequest('https://api.bithumb.com/public/ticker/BTC');
+//         request("https://api.coinone.co.kr/ticker?currency=btc", (error, response, body) => {
+//             if (error) {
+//                 console.log(error)
+//             }
+//             let data = JSON.parse(body);
+//             let price = parseInt(data.last);
+    
+//             if (last_price != -1) {
+//                 console.log(`比特币 price: ${price}, last: ${last_price}, 差价: ${price - last_price}`);
+//                 if (price < (last_price - 50000)) {
+//                     doSendEmail(price, "{BTC跌啦！}", {data: data, last: last_price, updown: -1});
+//                     console.log("BTC 跌破发送邮件！");
+//                     last_price = price;
+//                 } else if (price > (last_price + 50000)) {
+//                     doSendEmail(price, "[BTC涨啦]", {data: data, last: last_price, updown: 1});
+//                     console.log("BTC 涨幅发送邮件！");
+//                     last_price = price;
+//                 }
+//             } else {
+//                 last_price = price;
+//             }
+//         });
+
+//         request('https://api.coinone.co.kr/ticker?currency=bch', (error, response, body) => {
+//             if (error) {
+//                 console.log(error)
+//             }
+//             let data = JSON.parse(body);
+//             let price = parseInt(data.last);
+    
+//             if (last_bch_price != -1) {
+//                 console.log(`比特现金 price: ${price}, last: ${last_bch_price}, 差价: ${price - last_bch_price}`);
+//                 if (price < (last_bch_price - 20000)) {
+//                     doSendEmail(price, "{BCH:跌啦！}", {data: data, last: last_bch_price, updown: -1});
+//                     console.log("BCH 跌破发送邮件！");
+//                     last_bch_price = price;
+//                 } else if (price > (last_bch_price + 20000)) {
+//                     doSendEmail(price, "[BCH涨啦]", {data: data, last: last_bch_price, updown: 1});
+//                     console.log("BCH 涨幅发送邮件！");
+//                     last_bch_price = price;
+//                 }
+//             } else {
+//                 last_bch_price = price;
+//             }
+//         });
+        
+//     }, 3000);
+// }
+
+// scheduleCronstyle();
