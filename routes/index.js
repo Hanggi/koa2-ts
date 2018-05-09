25eb881de0dd3629db7fc71ed46166527e88f6c5
Hanggi <hanggicrown@gmail.com>
2018-05-09 21:57:42 +0900

    ng-admin

--- routes/index.js
+++ routes/index.js
@@ -11,79 +11,79 @@ const render 		= require('koa-ejs');
 // let charts = require("./charts");
 
 module.exports = (app) => {
-    // koa-ejs
-    render(app, {
-        root: path.join(__dirname, 'views'),
-        layout: null,
-        viewExt: 'html',
-        cache: false,
-        debug: true
-    });
-    console.log("!!!!")
+	// koa-ejs
+	render(app, {
+		root: path.join(__dirname, '../views'),
+		layout: null,
+		viewExt: 'html',
+		cache: false,
+		debug: true
+	});
+	console.log("!!!!")
 
-    router
-        .get('/', async (ctx, next) => {
+	router
+		.get('/', async (ctx, next) => {
 
-            console.log("/");
-            ctx.redirect("http://blog.hanggi.me");
-            // ctx.body = "index"
-        })
-        .get('/test', async (ctx, next) => {
-            let query = JSON.stringify(ctx.query);
-            console.log(typeof(query));
-            ctx.body = `This is the test page GET:${query}`;
-        })
+			console.log("/");
+			ctx.redirect("http://blog.hanggi.me");
+			// ctx.body = "index"
+		})
+		.get('/test', async (ctx, next) => {
+			let query = JSON.stringify(ctx.query);
+			console.log(typeof(query));
+			ctx.body = `This is the test page GET:${query}`;
+		})
 
-        .get('/adm*', async (ctx, next) => {
-            console.log(__dirname);
-            // await send(ctx, '/public/d/index.html');
-            await ctx.render('index', {
-                name: 'ng-admin'
-            })
-        })
-        .get('/qwe', async (ctx, next) => {
-            await ctx.render('home', {
-                name: 'koa2 '
-            });
-        });
-        app.use(router.routes());
+		.get('/adm*', async (ctx, next) => {
+			console.log(__dirname);
+			// await send(ctx, '/public/d/index.html');
+			await ctx.render('ng-admin', {
+				name: 'ng-admin'
+			})
+		})
+		.get('/qwe', async (ctx, next) => {
+			await ctx.render('home', {
+				name: 'koa2 '
+			});
+		});
+		app.use(router.routes());
 
-    v1.prefix("/v1");
-    v1.get('/qqq', async (ctx, next) => {
-        ctx.body = "qqq"
-    })
+		v1.prefix("/v1");
+		v1.get('/qqq', async (ctx, next) => {
+			ctx.body = "qqq"
+		})
 
 
-    app.use(router.routes());
-    app.use(v1.routes());
-    app.use(router.allowedMethods());
+	app.use(router.routes());
+	app.use(v1.routes());
+	app.use(router.allowedMethods());
 }
 
 
 function getRequest(url) {
-    return new Promise(function (resolve, reject) {
-        request(url, function (error, res, body) {
-            if (!error && res.statusCode == 200) {
-                resolve(body);
-            } else {
-                reject(error);
-            }
-        });
-    });
+	return new Promise(function (resolve, reject) {
+		request(url, function (error, res, body) {
+			if (!error && res.statusCode == 200) {
+				resolve(body);
+			} else {
+				reject(error);
+			}
+		});
+	});
 }
 function postRequest(options) {
-    return new Promise(function (resolve, reject) {
-        request.post(options, function (error, res, body) {
-            if (!error && res.statusCode == 200) {
-                resolve(body);
-            } else {
-                reject(error);
-            }
-        });
-    });
+	return new Promise(function (resolve, reject) {
+		request.post(options, function (error, res, body) {
+			if (!error && res.statusCode == 200) {
+				resolve(body);
+			} else {
+				reject(error);
+			}
+		});
+	});
 }
 
 function dateDisplayed(timestamp) {
-    let date = new Date(timestamp);
-    return (date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear());
+	let date = new Date(timestamp);
+	return (date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear());
 }
